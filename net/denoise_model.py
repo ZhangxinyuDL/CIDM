3da1379dede67a9465676a6108276ff044026811
ZhangxinyuDL <108869535+ZhangxinyuDL@users.noreply.github.com>
2025-02-02 22:22:58 +0800

    Update denoise_model.py

--- net/denoise_model.py
+++ net/denoise_model.py
@@ -1,1 +1,277 @@
+import math
+import torch
+from torch import nn
+from torch.nn import init
+from torch.nn import functional as F
+import einops
+
+
+def attention_einsum(q, k, v):
+    """Apply the attention operator to tensors of shape [h, w, c]."""
+
+    # Reshape 3D tensors to 2D tensor with first dimension L = h x w.
+    k = einops.rearrange(k, "h w c -> (h w) c")  # [h, w, c] -> [L, c]
+    v = einops.rearrange(v, "h w c -> (h w) c")  # [h, w, c] -> [L, c]
+
+    # Einstein summation corresponding to the query * key operation.
+    beta = F.softmax(torch.einsum("hwc, Lc->hwL", q, k), dim=-1)
+
+    # Einstein summation corresponding to the attention * value operation.
+    out = torch.einsum("hwL, Lc->hwc", beta, v)
+    return out
+
+
+class AttentionLayer(torch.nn.Module):
+    """Attention Module"""
+
+    def __init__(self, input_channels: int, output_channels: int, ratio_kq=8, ratio_v=8):
+        super(AttentionLayer, self).__init__()
+
+        self.ratio_kq = ratio_kq
+        self.ratio_v = ratio_v
+        self.output_channels = output_channels
+        self.input_channels = input_channels
+
+        # Compute query, key and value using 1x1 convolutions.
+        self.query = torch.nn.Conv2d(
+            in_channels=input_channels,
+            out_channels=self.output_channels // self.ratio_kq,
+            kernel_size=(1, 1),
+            padding="valid",
+            bias=False,
+        )
+        self.key = torch.nn.Conv2d(
+            in_channels=input_channels,
+            out_channels=self.output_channels // self.ratio_kq,
+            kernel_size=(1, 1),
+            padding="valid",
+            bias=False,
+        )
+        self.value = torch.nn.Conv2d(
+            in_channels=input_channels,
+            out_channels=self.output_channels // self.ratio_v,
+            kernel_size=(1, 1),
+            padding="valid",
+            bias=False,
+        )
+
+        self.last_conv = torch.nn.Conv2d(
+            in_channels=self.output_channels // 8,
+            out_channels=self.output_channels,
+            kernel_size=(1, 1),
+            padding="valid",
+            bias=False,
+        )
+
+        # Learnable gain parameter
+        self.gamma = nn.Parameter(torch.zeros(1))
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        # Compute query, key and value using 1x1 convolutions.
+        query = self.query(x)
+        # print(query.size())
+        key = self.key(x)
+        value = self.value(x)
+        # Apply the attention operation.
+        out = []
+        for b in range(x.shape[0]):
+            # Apply to each in batch
+            out.append(attention_einsum(query[b], key[b], value[b]))
+        out = torch.stack(out, dim=0)
+        out = self.gamma * self.last_conv(out)
+        # Residual connection.
+        return out + x
+
+
+class Swish(nn.Module):
+    def forward(self, x):
+        return x * torch.sigmoid(x)
+
+
+class TimeEmbedding(nn.Module):
+    def __init__(self, T, d_model, dim):
+        assert d_model % 2 == 0
+        super().__init__()
+        emb = torch.arange(0, d_model, step=2) / d_model * math.log(10000)
+        emb = torch.exp(-emb)
+        pos = torch.arange(T).float()
+        emb = pos[:, None] * emb[None, :]
+        assert list(emb.shape) == [T, d_model // 2]
+        emb = torch.stack([torch.sin(emb), torch.cos(emb)], dim=-1)
+        assert list(emb.shape) == [T, d_model // 2, 2]
+        emb = emb.view(T, d_model)
+
+        self.timembedding = nn.Sequential(
+            nn.Embedding.from_pretrained(emb),
+            nn.Linear(d_model, dim),
+            Swish(),
+            nn.Linear(dim, dim),
+        )
+        self.initialize()
+
+    def initialize(self):
+        for module in self.modules():
+            if isinstance(module, nn.Linear):
+                init.xavier_uniform_(module.weight)
+                init.zeros_(module.bias)
+
+    def forward(self, t):
+        emb = self.timembedding(t)
+        return emb
+
+
+class DownSample(nn.Module):
+    def __init__(self, in_ch):
+        super().__init__()
+        self.main = nn.Conv2d(in_ch, in_ch, 3, stride=2, padding=1)
+        self.initialize()
+
+    def initialize(self):
+        init.xavier_uniform_(self.main.weight)
+        init.zeros_(self.main.bias)
+
+    def forward(self, x, temb):
+        x = self.main(x)
+        return x
+
+
+class UpSample(nn.Module):
+    def __init__(self, in_ch):
+        super().__init__()
+        self.main = nn.Conv2d(in_ch, in_ch, 3, stride=1, padding=1)
+        self.initialize()
+
+    def initialize(self):
+        init.xavier_uniform_(self.main.weight)
+        init.zeros_(self.main.bias)
+
+    def forward(self, x, temb):
+        _, _, H, W = x.shape
+        x = F.interpolate(
+            x, scale_factor=2, mode='nearest')
+        x = self.main(x)
+        return x
+
+
+
+class ResBlock(nn.Module):
+    def __init__(self, in_ch, out_ch, tdim, dropout, attn=False):
+        super().__init__()
+        self.block1 = nn.Sequential(
+            nn.GroupNorm(32, in_ch),
+            Swish(),
+            nn.Conv2d(in_ch, out_ch, 3, stride=1, padding=1),
+        )
+        self.temb_proj = nn.Sequential(
+            Swish(),
+            nn.Linear(tdim, out_ch),
+        )
+        self.block2 = nn.Sequential(
+            nn.GroupNorm(32, out_ch),
+            Swish(),
+            nn.Dropout(dropout),
+            nn.Conv2d(out_ch, out_ch, 3, stride=1, padding=1),
+        )
+        if in_ch != out_ch:
+            self.shortcut = nn.Conv2d(in_ch, out_ch, 1, stride=1, padding=0)
+        else:
+            self.shortcut = nn.Identity()
+        if attn:
+            self.attn = AttentionLayer(out_ch,out_ch)
+        else:
+            self.attn = nn.Identity()
+        # self.initialize()
+
+    def initialize(self):
+        for module in self.modules():
+            if isinstance(module, (nn.Conv2d, nn.Linear)):
+                init.xavier_uniform_(module.weight)
+                init.zeros_(module.bias)
+        init.xavier_uniform_(self.block2[-1].weight, gain=1e-5)
+
+    def forward(self, x, temb):
+        h = self.block1(x)
+        h += self.temb_proj(temb)[:, :, None, None]
+        h = self.block2(h)
+
+        h = h + self.shortcut(x)
+        h = self.attn(h)
+        return h
+
+
+class UNet(nn.Module):
+    def __init__(self, T, ch, ch_mult, attn, num_res_blocks, dropout):
+        super().__init__()
+        assert all([i < len(ch_mult) for i in attn]), 'attn index out of bound'
+        tdim = ch * 4
+        self.time_embedding = TimeEmbedding(T, ch, tdim)
+
+        self.head = nn.Conv2d(1, ch, kernel_size=3, stride=1, padding=1)
+        self.downblocks = nn.ModuleList()
+        chs = [ch]  # record output channel when dowmsample for upsample
+        now_ch = ch
+        for i, mult in enumerate(ch_mult):
+            out_ch = ch * mult
+            for _ in range(num_res_blocks):
+                self.downblocks.append(ResBlock(
+                    in_ch=now_ch, out_ch=out_ch, tdim=tdim,
+                    dropout=dropout, attn=(i in attn)))
+                now_ch = out_ch
+                chs.append(now_ch)
+            if i != len(ch_mult) - 1:
+                self.downblocks.append(DownSample(now_ch))
+                chs.append(now_ch)
+
+        self.middleblocks = nn.ModuleList([
+            ResBlock(now_ch, now_ch, tdim, dropout, attn=True),
+            ResBlock(now_ch, now_ch, tdim, dropout, attn=False),
+        ])
+
+        self.upblocks = nn.ModuleList()
+        for i, mult in reversed(list(enumerate(ch_mult))):
+            out_ch = ch * mult
+            for _ in range(num_res_blocks + 1):
+                self.upblocks.append(ResBlock(
+                    in_ch=chs.pop() + now_ch, out_ch=out_ch, tdim=tdim,
+                    dropout=dropout, attn=(i in attn)))
+                now_ch = out_ch
+            if i != 0:
+                self.upblocks.append(UpSample(now_ch))
+        assert len(chs) == 0
+
+        self.tail = nn.Sequential(
+            nn.GroupNorm(32, now_ch),
+            Swish(),
+            nn.Conv2d(now_ch, 1, 3, stride=1, padding=1)
+        )
+        self.initialize()
+
+    def initialize(self):
+        init.xavier_uniform_(self.head.weight)
+        init.zeros_(self.head.bias)
+        init.xavier_uniform_(self.tail[-1].weight, gain=1e-5)
+        init.zeros_(self.tail[-1].bias)
+
+    def forward(self, x, t):
+        # Timestep embedding
+        temb = self.time_embedding(t)
+        # Downsampling
+        h = self.head(x)
+        hs = [h]
+        for layer in self.downblocks:
+            h = layer(h, temb)
+            hs.append(h)
+        # Middle
+        for layer in self.middleblocks:
+            h = layer(h, temb)
+        # Upsampling
+        for layer in self.upblocks:
+            if isinstance(layer, ResBlock):
+                h = torch.cat([h, hs.pop()], dim=1)
+            h = layer(h, temb)
+        h = self.tail(h)
+
+        assert len(hs) == 0
+        return h
+
 
